082b3df692aaa52a977f4385b62956e7919c81ad
Jose Palomares Cohen <joseangelcohen123@gmail.com>
2023-08-22 18:53:29 -0700

    Agregar archivos de proyecto.

---
+++ paralelo_PROYECTO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace paralelo_PROYECTO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\Users\Jose\Documents\Nueva carpeta\";
+            string path2 = @"C:\Users\Jose\Documents\Nueva carpeta (2)\";
+
+            string[] lst = Directory.GetFiles(path1);
+            Random r = new Random(1000);
+            Parallel.ForEach(lst, (archivo) =>
+            {
+                r.Next();
+                int a = r.Next();
+
+                File.Copy(archivo, path2 + "a" + a + ".txt");
+            });
+          
+
+        }
+    }
+}
